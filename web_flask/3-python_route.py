ff9dc8bfbce9d7745ef41af239a1adb2c449dfae
Your Username <amyne.boutallagh@gmail.com>
2024-02-17 07:34:00 -0800

    Done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,34 +2,36 @@
 """
 starts a Flask web application
 """
-
 from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello_hbnb():
+        """Display “Hello HBNB!”"""
+        return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+def display_hbnb():
+        """Display “HBNB”"""
+        return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def display_c(text):
+        """Display “C ”, followed by the value of the text variable"""
+        return 'C {}'.format(escape(text.replace('_', ' ')))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+def display_python(text):
+        """Display “Python ”, followed by the value of the text variable"""
+        return 'Python {}'.format(escape(text.replace('_', ' ')))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
