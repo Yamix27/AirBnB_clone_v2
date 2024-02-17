25dea2139b75b889571dc52493274ad09b7e739c
Your Username <amyne.boutallagh@gmail.com>
2024-02-17 09:13:37 -0800

    Done

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -9,9 +9,9 @@ Routes:
 /python/(<text>): Displays 'Python' followed by the value of <text>.
 /number/<n>: Displays 'n is a number' only if <n> is an integer.
 /number_template/<n>: Displays an HTML page only if <n> is an integer.
--Displays the value of <n> in the body.
+- Displays the value of <n> in the body.
 /number_odd_or_even/<n>: Displays an HTML page only if <n> is an integer.
--States whether <n> is even or odd in the body.
+- States whether <n> is even or odd in the body.
 """
 from flask import Flask
 from flask import render_template
