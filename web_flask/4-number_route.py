0bd82be6c62a7ca6aa3b1026970aaa1ade083da6
Your Username <amyne.boutallagh@gmail.com>
2024-02-17 07:47:04 -0800

    Done

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+        /: Displays 'Hello HBNB!'.
+            /hbnb: Displays 'HBNB'.
+                /c/<text>: Displays 'C' followed by the value of <text>.
+                    /python/(<text>): Displays 'Python' followed by the value of <text>.
+                        /number/<n>: Displays 'n is a number' only if <n> is an integer.
+                        """
 from flask import Flask
 from markupsafe import escape
 
